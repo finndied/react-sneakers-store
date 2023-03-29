edd95466f4cb242f84b17c342f73277772538a8f
Ilya Morozov <finnlivefx1@mail.ru>
2023-03-29 16:10:23 +0700

    Header layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className='wrapper'>
+			<header>
+				<div className='headerLeft'>
+					<img src="/img/logo.svg" width="60px"/>
+					<div>
+						<h3>Sneakers</h3>
+						<p>Store of the best sneakers</p>
+					</div>
+				</div>
+				<ul className='headerRight'>
+					<li>
+          <img src="/img/cart.svg" width="34px"/>
+						<span>0 руб.</span>
+					</li>
+					<li>
+          <img src="/img/user.svg" width="34px"/>
+					</li>
+				</ul>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
