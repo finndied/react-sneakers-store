1a84ddc1bc003654d039e854f8bec82633aaa548
Ilya Morozov <finnlivefx1@mail.ru>
2023-04-05 20:23:37 +0700

    Added text when there is no product in the cart

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -5,34 +5,43 @@ const Drawer = ({ total, onRemoveItem, items = [], opened }) => {
 	return (
 		<div className={`${styles.overlay} ${opened ? styles.overlayVisible : ''}`}>
 			<div className={styles.drawer}>
-				<div className={styles.items}>
-					{items.map(obj => (
-						<div key={obj.id} className={styles.cartItem}>
-							<img src={obj.imageUrl} width='100px' alt='Sneakers' />
-							<div>
-								<p>{obj.title}</p>
-								<p>{obj.price}</p>
-							</div>
-							<img
-								onClick={() => onRemoveItem(obj.id)}
-								className={styles.removeBtn}
-								src='/img/plus.svg'
-								width='30px'
-								alt='remove'
-							/>
+				{items.length === 0 ? (
+					<div className={styles.emptyCart}>
+						<img src='img/EmptyCart.png' alt='Empty cart' />
+						<p>Your cart is empty</p>
+					</div>
+				) : (
+					<>
+						<div className={styles.items}>
+							{items.map(obj => (
+								<div key={obj.id} className={styles.cartItem}>
+									<img src={obj.imageUrl} width='100px' alt='Sneakers' />
+									<div>
+										<p>{obj.title}</p>
+										<p>{obj.price}</p>
+									</div>
+									<img
+										onClick={() => onRemoveItem(obj.id)}
+										className={styles.removeBtn}
+										src='/img/plus.svg'
+										width='30px'
+										alt='remove'
+									/>
+								</div>
+							))}
 						</div>
-					))}
-				</div>
-				<div className={styles.totalBlock}>
-					<ul>
-						<li className={styles.priceCount}>
-							<span>Total:</span>
-							<div></div>
-							<b>{total}$</b>
-						</li>
-					</ul>
-					<button className={styles.checkBtn}>Checkout</button>
-				</div>
+						<div className={styles.totalBlock}>
+							<ul>
+								<li className={styles.priceCount}>
+									<span>Total:</span>
+									<div></div>
+									<b>{total}$</b>
+								</li>
+							</ul>
+							<button className={styles.checkBtn}>Checkout</button>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	)
